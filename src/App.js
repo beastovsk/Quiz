759ed1ab33e0d57550f9f1f3ea6089c17d7beae1
Artyom <narshinovartyom@mail.ru>
2021-11-30 18:18:51 +0400

    added css modules

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ class App extends Component {
 
 
     render() {
-        
+
         return (
             <Layout>
-
-
                 <div style={{width: 400, border: '1px solid #ccc'}}>
                     <h1>Layout</h1>
                 </div>
