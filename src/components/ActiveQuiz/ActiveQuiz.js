0ef1a456b23e6d6cc4d5a92eccb4787bd42619bd
Artyom <narshinovartyom@mail.ru>
2021-12-01 17:42:42 +0400

    response state

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -17,6 +17,7 @@ const ActiveQuiz = props => {
 
 
         <AnswersList 
+            state={props.state}
             answers={props.answers}
             onAnswerClick={props.onAnswerClick}
         />
