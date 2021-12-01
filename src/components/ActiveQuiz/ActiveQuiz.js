c904a82f374104893348b394b65bedf785fb0600
Artyom <narshinovartyom@mail.ru>
2021-12-01 07:04:39 +0400

    active question

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,1 +1,25 @@
-import React from "react";
+import React from "react"
+import classes from './ActiveQuiz.module.css'
+
+const ActiveQuiz = props => (
+    <div className={classes.ActiveQuiz}>
+        <p className={classes.Question}>
+            <span>
+                <strong>2.</strong> &nbsp;
+                Question?
+            </span>
+
+            <small>4/12</small>
+        </p>
+
+
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </div>
+)
+
+export default ActiveQuiz
