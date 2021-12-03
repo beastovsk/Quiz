274aca9ed915e72923e9e8914fc74119befe0fd4
Artyom <narshinovartyom@mail.ru>
2021-12-03 08:29:41 +0400

    closing burger on click to layout

--- src/components/Navigation/MenuToogle/Drawer/Drawer.js
+++ src/components/Navigation/MenuToogle/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Drawer.module.css'
+import Backdrop from '../../../UI/Button/Backdrop/Backdrop';
 
 
 const links = [
@@ -20,6 +21,7 @@ class Drawer extends Component {
     }
 
 
+
     render() {
         const cls = [classes.Drawer]
 
@@ -28,11 +30,14 @@ class Drawer extends Component {
         }
 
         return (
-            <nav className={cls.join(' ')}>
-                <ul>
-                    {this.renderLinks()}
-                </ul>
-            </nav>
+            <React.Fragment>
+                <nav className={cls.join(' ')}>
+                    <ul>
+                        {this.renderLinks()}
+                    </ul>
+                </nav>
+                {this.props.isOpen ? <Backdrop onClick={ this.props.onClose }/> : null} 
+            </React.Fragment>
         )
     }
 }
